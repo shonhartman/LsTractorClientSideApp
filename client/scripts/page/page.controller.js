41425407533a7ae11640e2d1eb2d41c36593a60b
Jonathan Motes <Jonathan Motes>
2016-11-25 09:56:12 -0600

    Completed login code for new /login endpoint.

--- client/scripts/page/page.controller.js
+++ client/scripts/page/page.controller.js
@@ -27,12 +27,12 @@
         // Dealership Owner - 5
         // Super Admin - 6
 
-//OLD LOGIN FUNCTION
+        //OLD LOGIN FUNCTION
         // $scope.login = function () {
 
         //     if (!$scope.formData) {
         //         return;
-        //     }            
+        //     }
 
         //     $http.get('http://lstractor.southcentralus.cloudapp.azure.com:8080/tractor-quiz-api/appUsers')
         //         .then(function (response) {
@@ -58,25 +58,43 @@
         //         });
         // }
 
-//NEW LOGIN FUNCTION
+        //NEW LOGIN FUNCTION
         $scope.login = function () {
 
             if (!$scope.formData) {
                 return;
-            }            
+            }
 
             $http.post('http://lstractor.southcentralus.cloudapp.azure.com:8080/tractor-quiz-api/login', {
-                "email" : $scope.formData.Username,
-                "password" : $scope.formData.Password
-            })
-            .then(function(response) {
-                    $scope.userId = response.data.userId;
-                     console.log($scope.userId);
-            });
-                    
-                    // $cookies.put('user', angular.toJson(user));
-                    // $location.url('/dashboard');
-                
+                    "email": $scope.formData.Username,
+                    "password": $scope.formData.Password
+                })
+                .then(function (response) {
+
+                    if (!response.data.ok) {
+
+                        // authentication failed - need to show login error message
+
+                    } else {
+
+                        var userId = response.data.userId;
+                        $scope.userId = userId;
+
+                        // now do a request for the user info and save it to cookie
+                        $http.get('http://lstractor.southcentralus.cloudapp.azure.com:8080/tractor-quiz-api/appUsers/' + userId)
+                            .then(function (response) {
+
+                                var user = response.data;
+
+                                if (!user) {
+                                    return;
+                                }
+
+                                $cookies.put('user', angular.toJson(user));
+                                $location.url('/dashboard');
+                            });
+                    }
+                });
         }
 
         //SAVES USER TO DATABASE
@@ -84,8 +102,7 @@
             console.log($scope.formData);
 
             // $location.url('/dashboard')
-            $http.post('http://lstractor.southcentralus.cloudapp.azure.com:8080/tractor-quiz-api/appUsers',
-                {
+            $http.post('http://lstractor.southcentralus.cloudapp.azure.com:8080/tractor-quiz-api/appUsers', {
                     "firstName": $scope.formData.FirstName,
                     "lastName": $scope.formData.LastName,
                     "email": $scope.formData.Email,
@@ -104,9 +121,3 @@
     }
 
 })();
-
-
-
-
-
-
