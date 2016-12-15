fea6224db1b550b8d7d70e92888c99af59957a59
Jonathan Motes <Jonathan Motes>
2016-12-15 17:01:09 -0600

    Show skillsets list again.

--- client/scripts/skillset/skillset.controller.js
+++ client/scripts/skillset/skillset.controller.js
@@ -6,118 +6,120 @@
 
 
     function SkillSetCtrl($cookies, $modal, $scope, $http, $routeParams, logger, $location) {
-       
-//GET SKILLSET BY ID
+
+        //GET SKILLSET BY ID
         $scope.skillsets = [];
 
-        $http.get('http://lstractorquizapi.azurewebsites.net/api/skillSets')
+        var user = angular.fromJson($cookies.get('user'));
+
+        $http.get('http://lstractorquizapi.azurewebsites.net/api/SkillSets')
             .then(function (response) {
 
-                $scope.skillsets = [];
-                var user = angular.fromJson($cookies.get('user'));
+                $scope.skillsets = response.data;
 
-                response.data._embedded.skillSets.forEach(function (skillset) {
+                // response.data._embedded.skillSets.forEach(function (skillset) {
 
-                    var splitUrl = skillset._links.self.href.split("/");
-                    skillset.id = splitUrl[splitUrl.length - 1];
+                //     var splitUrl = skillset._links.self.href.split("/");
+                //     skillset.id = splitUrl[splitUrl.length - 1];
 
-                    // set skillset status
-                    // 1. not started
-                    // 2. in progress
-                    // 3. complete
+                //     // set skillset status
+                //     // 1. not started
+                //     // 2. in progress
+                //     // 3. complete
 
-                    var skillsetVideoIDs = [];
+                //     var skillsetVideoIDs = [];
 
-                    $http.get(skillset._links.listOfVideo.href)
-                        .then(function (response) {
+                //     $http.get(skillset._links.listOfVideo.href)
+                //         .then(function (response) {
 
-                            // 1. collect videos in this skillset
-                            skillsetVideoIDs = response.data._embedded.videos.map(function (video) {
-                                return video._links.self.href.split('/').pop();//What does this call look like??
-                            });
+                //             // 1. collect videos in this skillset
+                //             skillsetVideoIDs = response.data._embedded.videos.map(function (video) {
+                //                 return video._links.self.href.split('/').pop(); //What does this call look like??
+                //             });
 
-                            // 2. search for user video results
-                            return $http.get('http://lstractorquizapi.azurewebsites.net/api/SkillSets?userId={userId}&skillSetId={skillSetId}')
+                //             // 2. search for user video results
+                //             return $http.get('http://lstractorquizapi.azurewebsites.net/api/SkillSets?userId={userId}&skillSetId={skillSetId}')
 
-                        })
-                        .then(function (response) {
+                //         })
+                //         .then(function (response) {
 
-                            //3. get list of user video results that are marked as complete
-                            var userCompletedVideoResults = response.data._embedded.videoResults.filter(function (video) {
-                                return video.isComplete;//What does this call look like??
-                            });
+                //             //3. get list of user video results that are marked as complete
+                //             var userCompletedVideoResults = response.data._embedded.videoResults.filter(function (video) {
+                //                 return video.isComplete; //What does this call look like??
+                //             });
 
-                            var userVideosCompletedIDs = [];
-                            var videosInSkillCompletedCount = 0;
+                //             var userVideosCompletedIDs = [];
+                //             var videosInSkillCompletedCount = 0;
 
-                            function checkForSkillVideoInUserResults(userCompletedVideoResults, count) {
+                //             function checkForSkillVideoInUserResults(userCompletedVideoResults, count) {
 
-                                $http.get(userCompletedVideoResults[count]._links.video.href).then(function (response) {
+                //                 $http.get(userCompletedVideoResults[count]._links.video.href).then(function (response) {
 
-                                    //now, get the ids of completed videos
-                                    var videoCompletedID = response.data._links.self.href.split('/').pop();
+                //                     //now, get the ids of completed videos
+                //                     var videoCompletedID = response.data._links.self.href.split('/').pop();
 
-                                    //keep count of videos completed for the current skillset
-                                    if (skillsetVideoIDs.indexOf(videoCompletedID) >= 0) {
-                                        videosInSkillCompletedCount++;
-                                    }
+                //                     //keep count of videos completed for the current skillset
+                //                     if (skillsetVideoIDs.indexOf(videoCompletedID) >= 0) {
+                //                         videosInSkillCompletedCount++;
+                //                     }
 
-                                    if (userCompletedVideoResults.length > count + 1) {
-                                        checkForSkillVideoInUserResults(userCompletedVideoResults, count + 1);
-                                    } else {
+                //                     if (userCompletedVideoResults.length > count + 1) {
+                //                         checkForSkillVideoInUserResults(userCompletedVideoResults, count + 1);
+                //                     } else {
 
-                                        //4. set skillset status
-                                        if (skillsetVideoIDs.length > 0 && videosInSkillCompletedCount >= skillsetVideoIDs.length) {
-                                            skillset.status = 3;
-                                        } else if (videosInSkillCompletedCount > 0) {
-                                            skillset.status = 2;
-                                        } else {
-                                            skillset.status = 1;
-                                        }
+                //                         //4. set skillset status
+                //                         if (skillsetVideoIDs.length > 0 && videosInSkillCompletedCount >= skillsetVideoIDs.length) {
+                //                             skillset.status = 3;
+                //                         } else if (videosInSkillCompletedCount > 0) {
+                //                             skillset.status = 2;
+                //                         } else {
+                //                             skillset.status = 1;
+                //                         }
 
-                                        $scope.skillsets.push(skillset);
-                                    }
+                //                         $scope.skillsets.push(skillset);
+                //                     }
 
-                                }, function () {
-                                    //handle error
-                                    skillset.status = 1;
+                //                 }, function () {
+                //                     //handle error
+                //                     skillset.status = 1;
 
-                                    if (userCompletedVideoResults.length > count + 1) {
-                                        checkForSkillVideoInUserResults(userCompletedVideoResults, count + 1);
-                                    } else {
+                //                     if (userCompletedVideoResults.length > count + 1) {
+                //                         checkForSkillVideoInUserResults(userCompletedVideoResults, count + 1);
+                //                     } else {
 
-                                        //4. set skillset status
-                                        if (skillsetVideoIDs.length > 0 && videosInSkillCompletedCount >= skillsetVideoIDs.length) {
-                                            skillset.status = 3;
-                                        } else if (videosInSkillCompletedCount > 0) {
-                                            skillset.status = 2;
-                                        } else {
-                                            skillset.status = 1;
-                                        }
+                //                         //4. set skillset status
+                //                         if (skillsetVideoIDs.length > 0 && videosInSkillCompletedCount >= skillsetVideoIDs.length) {
+                //                             skillset.status = 3;
+                //                         } else if (videosInSkillCompletedCount > 0) {
+                //                             skillset.status = 2;
+                //                         } else {
+                //                             skillset.status = 1;
+                //                         }
 
-                                        $scope.skillsets.push(skillset);
-                                    }
-                                });
-                            }
+                //                         $scope.skillsets.push(skillset);
+                //                     }
+                //                 });
+                //             }
 
-                            checkForSkillVideoInUserResults(userCompletedVideoResults, 0);
-                        });
-                });
+                //             checkForSkillVideoInUserResults(userCompletedVideoResults, 0);
+                //         });
+                // });
             });
 
         $scope.goToSkillSet = function () {
             location.href = '#/pages/skill-set-details/{{ skillset.id }}';
         }
-//UPDATE SKILL SET NAME BY ID
+
+        //UPDATE SKILL SET NAME BY ID
         $scope.updateSkillset = function () {
             $http.put('http://lstractorquizapi.azurewebsites.net/api/SkillSets?skillSetId={skillSetId}&name={name}')
-                .then(function(response) {
+                .then(function (response) {
                     logger.logSuccess("Your Edit was successful");
                     $location.url("/#/skill-set");
                 })
         }
-        
-//DELETE SKILLSET BY ID
+
+        //DELETE SKILLSET BY ID
         $scope.deleteSkillSet = function () {
             $http.delete('http://lstractorquizapi.azurewebsites.net/api/SkillSets?skillSetId={skillSetId}')
                 .then(function (response) {
@@ -148,7 +150,5 @@
             });
         }
     }
-
-
 
 })();
