7238d11dd9c41f24612393728d09abf1f99eaa7d
Jonathan Motes <Jonathan Motes>
2016-12-20 01:00:56 -0600

    Real skillset status on employee dashboard report.

--- client/scripts/skillset/skillset.controller.js
+++ client/scripts/skillset/skillset.controller.js
@@ -7,108 +7,13 @@
 
     function SkillSetCtrl($cookies, $modal, $scope, $http, $routeParams, logger, $location) {
 
-        //GET SKILLSET BY ID
         $scope.skillsets = [];
-
-        var user = angular.fromJson($cookies.get('user'));
-
-        var skillSetId = $routeParams.id;
 
         $http.get($scope.main.apiUrl + 'SkillSets/GetAllSkillSets')
             .then(function (response) {
-
                 $scope.skillsets = response.data;
-
-                // response.data._embedded.skillSets.forEach(function (skillset) {
-
-                //     var splitUrl = skillset._links.self.href.split("/");
-                //     skillset.id = splitUrl[splitUrl.length - 1];
-
-                //     // set skillset status
-                //     // 1. not started
-                //     // 2. in progress
-                //     // 3. complete
-
-                //     var skillsetVideoIDs = [];
-
-                //     $http.get(skillset._links.listOfVideo.href)
-                //         .then(function (response) {
-
-                //             // 1. collect videos in this skillset
-                //             skillsetVideoIDs = response.data._embedded.videos.map(function (video) {
-                //                 return video._links.self.href.split('/').pop(); //What does this call look like??
-                //             });
-
-                //             // 2. search for user video results
-                //             return $http.get('http://lstractorquizapi.azurewebsites.net/api/SkillSets?userId={userId}&skillSetId={skillSetId}')
-
-                //         })
-                //         .then(function (response) {
-
-                //             //3. get list of user video results that are marked as complete
-                //             var userCompletedVideoResults = response.data._embedded.videoResults.filter(function (video) {
-                //                 return video.isComplete; //What does this call look like??
-                //             });
-
-                //             var userVideosCompletedIDs = [];
-                //             var videosInSkillCompletedCount = 0;
-
-                //             function checkForSkillVideoInUserResults(userCompletedVideoResults, count) {
-
-                //                 $http.get(userCompletedVideoResults[count]._links.video.href).then(function (response) {
-
-                //                     //now, get the ids of completed videos
-                //                     var videoCompletedID = response.data._links.self.href.split('/').pop();
-
-                //                     //keep count of videos completed for the current skillset
-                //                     if (skillsetVideoIDs.indexOf(videoCompletedID) >= 0) {
-                //                         videosInSkillCompletedCount++;
-                //                     }
-
-                //                     if (userCompletedVideoResults.length > count + 1) {
-                //                         checkForSkillVideoInUserResults(userCompletedVideoResults, count + 1);
-                //                     } else {
-
-                //                         //4. set skillset status
-                //                         if (skillsetVideoIDs.length > 0 && videosInSkillCompletedCount >= skillsetVideoIDs.length) {
-                //                             skillset.status = 3;
-                //                         } else if (videosInSkillCompletedCount > 0) {
-                //                             skillset.status = 2;
-                //                         } else {
-                //                             skillset.status = 1;
-                //                         }
-
-                //                         $scope.skillsets.push(skillset);
-                //                     }
-
-                //                 }, function () {
-                //                     //handle error
-                //                     skillset.status = 1;
-
-                //                     if (userCompletedVideoResults.length > count + 1) {
-                //                         checkForSkillVideoInUserResults(userCompletedVideoResults, count + 1);
-                //                     } else {
-
-                //                         //4. set skillset status
-                //                         if (skillsetVideoIDs.length > 0 && videosInSkillCompletedCount >= skillsetVideoIDs.length) {
-                //                             skillset.status = 3;
-                //                         } else if (videosInSkillCompletedCount > 0) {
-                //                             skillset.status = 2;
-                //                         } else {
-                //                             skillset.status = 1;
-                //                         }
-
-                //                         $scope.skillsets.push(skillset);
-                //                     }
-                //                 });
-                //             }
-
-                //             checkForSkillVideoInUserResults(userCompletedVideoResults, 0);
-                //         });
-                // });
+                setSkillsetsProgress();
             });
-
-
 
         $scope.goToSkillSet = function () {
             location.href = '#/pages/skill-set-details/{{ skillset.id }}';
@@ -163,8 +68,45 @@
             });
         }
 
+        function setSkillsetsProgress() {
 
+            angular.forEach($scope.skillsets, function (skillset) {
 
+                $http.get($scope.main.apiUrl + 'SkillSets/GetSkillSetResults', {
+                    params: {
+                        "skillSetId": skillset.Id,
+                        "userId": $scope.main.user.Id
+                    }
+                }).then(function (response) {
+
+                    var videoResults = response.data.VideoResults;
+                    var quizResults = response.data.QuizResults;
+
+                    // calculate overall skillset progress
+                    skillset.Progress = (quizResults.length / skillset.Quizzes.length) * 100;
+
+                    // set status on each quiz
+                    angular.forEach(skillset.Quizzes, function (quiz) {
+
+                        quiz.Status = 1; // not started
+
+                        var quizResult = quizResults.find(function (result) {
+                            return result.QuizId === quiz.Id;
+                        });
+
+                        var quizVideoProgress = videoResults.find(function (result) {
+                            return result.VideoId === quiz.Video.Id;
+                        });
+
+                        if (quizResult) {
+                            quiz.Status = 3; // complete
+                        } else if (quizVideoProgress && quizVideoProgress.ViewedAmount > 10) {
+                            quiz.Status = 2; // in progress
+                        }
+                    });
+                });
+            });
+        }
     }
 
 })();
